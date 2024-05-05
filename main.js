5c48c356bd5926cf2f309c7b362b63f03d4dd158
unknown <mitrovicbojan87@gmail.com>
2024-05-05 22:10:23 +0300

    faq

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+////////
+//tabs//
+////////
+
 const tabs = document.querySelectorAll("[data-tab-target");
 const tabContents = document.querySelectorAll("[data-tab-content]");
 
@@ -5,10 +9,16 @@ tabs.forEach((tab) => {
   tab.addEventListener("click", () => {
     const target = document.querySelector(tab.dataset.tabTarget);
 
+    //display tabs
     tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
 
+    //add active class to tab button
     tabs.forEach((tab) => tab.classList.remove("active"));
     tab.classList.add("active");
     target.classList.add("active");
   });
 });
+
+/////////////
+//accordion//
+/////////////
